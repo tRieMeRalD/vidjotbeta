6a291719b1b02daf412a0d960261ff2eaab2afb2
tRieMeRalD <will.lao1-05@outlook.com>
2019-02-22 21:35:17 -0500

    Second Commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: "mongodb+srv://tRiwnl:HorizonDank$1234@cluster0-w81bp.mongodb.net/test?retryWrites=true"}
+    module.exports = {mongoURI: "mongodb+srv://tRiwnl:HorizonDank%241234@cluster0-w81bp.mongodb.net/test?retryWrites=true"}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
