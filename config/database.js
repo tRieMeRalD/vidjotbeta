bfaba589688b876805f9c3b7fcb8999e81ce02ff
tRieMeRalD <will.lao1-05@outlook.com>
2019-02-22 21:43:12 -0500

    third commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: "mongodb+srv://tRiwnl:HorizonDank%241234@cluster0-w81bp.mongodb.net/test?retryWrites=true"}
+    module.exports = {mongoURI: "mongodb://tRiwnl:HorizonDank%241234@cluster0-shard-00-00-w81bp.mongodb.net:27017,cluster0-shard-00-01-w81bp.mongodb.net:27017,cluster0-shard-00-02-w81bp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
