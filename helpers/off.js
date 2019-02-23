ac0ebbcf456781e7473af4e870739666f249ec14
tRieMeRalD <will.lao1-05@outlook.com>
2019-02-23 18:50:06 -0500

    feb 23 initial commit

--- helpers/off.js
+++ helpers/off.js
@@ -3,7 +3,7 @@ module.exports = {
         if(req.isAuthenticated()) {
             return next();
         } 
-        req.flash('error_msg', 'Not Authorized');
+        req.flash('error_msg', 'Not Authorized. Please login first.');
         res.redirect('/users/login');
     }
 }
